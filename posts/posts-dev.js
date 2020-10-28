e37921398465f1e2c9af48598fe774de52db01e2
Sean Zhu <sean.zhu@nyu.edu>
2020-10-28 07:06:24 -0700

    Make live dev refresh less jarring

--- posts/posts-dev.js
+++ posts/posts-dev.js
@@ -2,22 +2,32 @@ import reformatDoc from "../meta/format-post-lib/reformatDoc.js";
 
 // reformatDoc(document, document.documentElement);
 
-async function reload() {
-  let response = await fetch(location.href);
-  let html = await response.text();
+async function reload(shouldReloadHead, signal) {
+  let response = await fetch(location.href, { signal });
+  let html = await response.text({ signal });
   let newDoc = document.createElement("html");
   newDoc.innerHTML = html;
   reformatDoc(document, newDoc);
 
-  document.head?.remove();
-  document.documentElement.append(newDoc.querySelector("head"));
+  document.documentElement.style.height = document.documentElement.scrollHeight;
   document.body?.remove();
+  if (shouldReloadHead) {
+    document.head?.remove();
+    document.documentElement.append(newDoc.querySelector("head"));
+    await new Promise(requestAnimationFrame);
+  }
   document.documentElement.append(newDoc.querySelector("body"));
+  document.documentElement.removeAttribute("style");
   hydrate();
 }
 
+let abortController;
 window.addEventListener("keypress", (e) => {
-  if (e.key === "r") {
-    reload();
+  if (e.key === "r" || e.key === "R") {
+    let shouldReloadHead = e.key === "R";
+    abortController?.abort();
+    abortController = new AbortController();
+    reload(shouldReloadHead, abortController.signal);
+    console.info("Successfully reloaded with", { shouldReloadHead });
   }
 });
